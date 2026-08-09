1284c3cf77e453abb8fd466cd09b15f7755410f7
ketidev <ketikhetsuriani21@gmail.com>
2026-08-09 23:41:57 +0400

    server

---
+++ server/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from middlewares.exception_handlers import catch_exception_middleware
+from routes.upload_pdfs import router as upload_router
+# from routes.ask_question import router as ask_router
+
+app = FastAPI(title="Medical Assistant API", version="1.0.0", description="An API for AI Medical Assistant Application.")
+
+# Cors Setup
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"], 
+    allow_methods=["*"], 
+    allow_headers=["*"]
+)
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello World"}
+
+# middleware exception handlers
+app.middleware("http")(catch_exception_middleware)
+
+# routers
+
+# 1. upload pdfs documents
+app.include_router(upload_router)
+# 2. asking query
+# app.include_router(ask_router)
